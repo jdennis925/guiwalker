521efd951785f4a9af7b0387b930637be3ee7663
john <jdennis925@gmail.com>
2013-08-19 13:32:46 -0500

    Added conceptual framework for GetElementPriority Algorithm in the ElementPicker.

---
+++ GUIWalker/ElementEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Automation;
+
+namespace GUIWalker
+{
+    //attribute class for logging AutomationElements.
+    public class ElementEntry
+    {
+        public string Name { get; set; }
+        public string AutomationId { get; set; }
+        public ControlType mControlType { get; set; }
+        public int Priority { get; set; }
+    }
+}
--- GUIWalker/ElementPicker.cs
+++ GUIWalker/ElementPicker.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
-using System.Threading;
 using System.Windows.Automation;
-using System.Runtime.InteropServices;
-using System.Windows;
-using System.IO;
+using System.Linq;
+using System.Diagnostics;
 
 namespace GUIWalker
 {
@@ -23,6 +18,102 @@ namespace GUIWalker
             if (SkipElement(inSet[index]))
                 return null;
             return inSet[index];
+        }
+
+
+        private static int PrioritizeElementEntries(ElementEntry left, ElementEntry right)
+        {
+            if (left == null)
+            {
+                return right == null ? 0 : -1;
+            }
+            else if (right == null)
+            {
+                return 1;
+            }
+            else
+            {
+                if (left.Priority == right.Priority)
+                    return 0;
+                return left.Priority > right.Priority ? 1 : -1;
+            }
+            Debug.Assert(false);
+            return 0;
+        }
+
+        List<ElementEntry> mElementTracker = new List<ElementEntry> { };
+        public AutomationElement GetElementPriority(AutomationElementCollection inCollection)
+        {
+            //gather the current elements in the GUI.
+            List<ElementEntry> currentElements = new List<ElementEntry> { };
+            foreach (AutomationElement iter in inCollection)
+            {
+                if (SkipElement(iter))
+                    continue;
+
+                var elementLog =
+                    from myElem in mElementTracker
+                    where (myElem.Name == iter.Current.Name)
+                    select myElem;
+
+                if (elementLog.Count() == 0)
+                { //new element
+                    mElementTracker.Add(new ElementEntry { Name = iter.Current.Name, AutomationId = iter.Current.AutomationId, mControlType = iter.Current.ControlType, Priority = 100 });
+                }
+                if (elementLog.Count() <= 1)
+                { //recover old element
+                    currentElements.Add(elementLog.First());
+                }
+                else
+                { //multiple matches to element. Add more criteria.
+                    Debug.Assert(false);
+                }
+            }
+
+            //sort current elements by rank.
+            currentElements.Sort(PrioritizeElementEntries);
+
+            //
+            int sumPriority = 0;
+            foreach (ElementEntry elem in currentElements)
+            {
+                sumPriority += elem.Priority;
+            }
+
+            //randomly choose a ElementEntry weighted by priority.
+            AutomationElement returnElement = null;
+            int randomVal = rand.Next(sumPriority);
+            int count = randomVal;
+            foreach (ElementEntry elem in currentElements)
+            {
+                if (count < elem.Priority && returnElement == null)
+                {
+                    elem.Priority--;
+                    var elementLog =
+                    from AutomationElement myElem in inCollection
+                    where (myElem.Current.Name == elem.Name)
+                    select myElem;
+                    returnElement = elementLog.First();
+                }
+                else
+                {
+                    count -= elem.Priority;
+                    elem.Priority++;
+                }
+
+                    var updateLog =
+                    from myElem in mElementTracker
+                    where (myElem.Name == elem.Name)
+                    select myElem;
+                
+                    updateLog.First().Priority = elem.Priority;
+            }
+
+
+
+
+
+            return returnElement;
         }
 
         //names of elements we intend not to interact with.
